ee92ec268eab8819bc4c09052d94d3c632a7b34f
Sean Baines <mrseanbaines@gmail.com>
2019-11-10 17:35:26 +0100

    Add middleware & adjust a couple of eslint rules

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,22 @@
+/* eslint-disable import/first */
 require('dotenv-safe').config();
 
-// eslint-disable-next-line import/first
 import express from 'express';
-
-// eslint-disable-next-line import/first
 import * as routes from './routes';
+/* eslint-enable import/first */
 
 const app = express();
 const { PORT, SITE_URL } = process.env;
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', SITE_URL);
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   next();
 });
+
+app.use(express.json());
 
 app.use('/auth', routes.auth);
 
