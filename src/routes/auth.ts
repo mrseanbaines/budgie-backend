9f55ed357f9dd17ae76a16452b762dd3e16217ca
Sean Baines <mrseanbaines@gmail.com>
2020-02-12 23:06:41 +0000

    Refactor auth flow

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,22 +1,49 @@
 import express from 'express'
 import axios from 'axios'
 import { stringify } from 'querystring'
+import uuidv4 from 'uuid/v4'
 
 const router = express.Router()
-const { MONZO_BASE_URL = '', CLIENT_SECRET = '' } = process.env
+const {
+  MONZO_BASE_URL = '',
+  MONZO_AUTH_URL = '',
+  CLIENT_SECRET = '',
+  CLIENT_ID = '',
+  SITE_URL = '',
+  REDIRECT_URI = '',
+} = process.env
 
-router.get('/token', async (req, res) => {
+const uuid = uuidv4()
+
+router.get('/login', (req, res) => {
+  res.redirect(
+    `${MONZO_AUTH_URL}?${stringify({
+      client_id: CLIENT_ID,
+      redirect_uri: REDIRECT_URI,
+      response_type: 'code',
+      state: uuid,
+    })}`,
+  )
+})
+
+router.get('/callback', async (req, res) => {
+  const { code, state } = req.query
+
+  if (state !== uuid) {
+    return res.end()
+  }
+
   const body = {
+    grant_type: 'authorization_code',
+    client_id: CLIENT_ID,
     client_secret: CLIENT_SECRET,
-    grant_type: req.query.grant_type,
-    client_id: req.query.client_id,
-    redirect_uri: req.query.redirect_uri,
-    code: req.query.code,
+    redirect_uri: REDIRECT_URI,
+    code,
   }
 
   const response = await axios.post(`${MONZO_BASE_URL}/oauth2/token`, stringify(body))
 
-  res.status(200).send({ accessToken: response.data.access_token })
+  return res.redirect(`${SITE_URL}?access_token=${response.data.access_token}`)
 })
 
 export default router
