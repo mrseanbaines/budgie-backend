31f5c7344676d0e0f22981b22fd300dbb9f0fdd1
Sean Baines <mrseanbaines@gmail.com>
2020-03-22 13:12:01 +0000

    Use db for delete category endpoint & wrap endpoints with try/catch

--- src/routes/categories.ts
+++ src/routes/categories.ts
@@ -1,65 +1,74 @@
 import express from 'express'
 
-import data from '../../data'
 import Category from '../models/category-model'
 
 const router = express.Router()
 
 // List Categories
 router.get('/', async (req, res) => {
-  const limit = parseInt(req.query.limit, 10) || 25
-  const offset = parseInt(req.query.offset, 10) || 0
+  try {
+    const limit = parseInt(req.query.limit, 10) || 25
+    const offset = parseInt(req.query.offset, 10) || 0
 
-  const categories = await Category.find()
+    const categories = await Category.find()
 
-  return res.status(200).send({
-    items: categories.slice(offset, offset + limit),
-    total: categories.length,
-  })
+    return res.status(200).send({
+      items: categories.slice(offset, offset + limit),
+      total: categories.length,
+    })
+  } catch (err) {
+    return res.status(500).send(err)
+  }
 })
 
 // Add Category
 router.post('/', async (req, res) => {
-  const category = req.body
-
-  if (!category) {
-    return res.status(422).send('No category provided')
-  }
-
-  const newCategory = {
-    name: category.name.trim(),
-  }
-
-  const existingCategory = await Category.findOne({
-    name: { $regex: new RegExp(`^${newCategory.name}$`, 'i') },
-  })
-
-  if (existingCategory) {
-    return res.status(409).send('Category already exists')
-  }
-
   try {
+    const category = req.body
+
+    if (!category) {
+      return res.status(422).send('No category provided')
+    }
+
+    const newCategory = {
+      name: category.name.trim(),
+    }
+
+    const existingCategory = await Category.findOne({
+      name: { $regex: new RegExp(`^${newCategory.name}$`, 'i') },
+    })
+
+    if (existingCategory) {
+      return res.status(409).send('Category already exists')
+    }
+
     await new Category(newCategory).save()
+
+    const categoryCount = await Category.estimatedDocumentCount()
+
+    return res.status(201).send({ category: newCategory, total: categoryCount })
   } catch (err) {
-    return res.status(500).send('Error creating category')
+    return res.status(500).send(err)
   }
-
-  const categoryCount = await Category.estimatedDocumentCount()
-
-  return res.status(201).send({ category: newCategory, total: categoryCount })
 })
 
 // Delete Category
-router.delete('/:id', (req, res) => {
-  const existingCategory = data.categories.find(({ id }) => id === req.params.id)
+router.delete('/:id', async (req, res) => {
+  try {
+    const existingCategory = await Category.findById(req.params.id)
 
-  if (!existingCategory) {
-    return res.status(404).send('Category not found')
+    if (!existingCategory) {
+      return res.status(404).send('Category not found')
+    }
+
+    existingCategory.remove()
+
+    const categoryCount = await Category.estimatedDocumentCount()
+
+    return res.status(200).send({ category: existingCategory, total: categoryCount })
+  } catch (err) {
+    return res.status(500).send(err)
   }
-
-  data.categories = data.categories.filter(category => category !== existingCategory)
-
-  return res.status(200).send({ category: existingCategory, total: data.categories.length })
 })
 
 export default router
