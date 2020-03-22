7c05692a3c6cde58f750b639ad3fa26fb3afe858
Sean Baines <mrseanbaines@gmail.com>
2020-03-22 16:38:26 +0000

    Use category types in model

--- src/models/category-model.ts
+++ src/models/category-model.ts
@@ -1,10 +1,11 @@
 import mongoose from 'mongoose'
+import * as types from '../types'
 
 const categorySchema = new mongoose.Schema({
   name: String,
   color: String,
 })
 
-const Category = mongoose.model('Category', categorySchema)
+const Category = mongoose.model<types.Category>('Category', categorySchema)
 
 export default Category
