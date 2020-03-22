86a96610d6a5db0358b44fc47278f3206991d89e
Sean Baines <mrseanbaines@gmail.com>
2020-03-22 16:44:25 +0000

    Add delete allowed header

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ connectDatabase()
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', SITE_URL)
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
 
   next()
