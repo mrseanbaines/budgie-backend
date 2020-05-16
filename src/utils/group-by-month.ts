d40c0305489d7224eb7825b6804c0be5c218ca8e
Sean Baines <mrseanbaines@gmail.com>
2020-05-16 13:51:46 +0100

    Order transactions before grouping

--- src/utils/group-by-month.ts
+++ src/utils/group-by-month.ts
@@ -1,11 +1,17 @@
 import { isSameMonth } from 'date-fns'
-import { groupWith } from 'ramda'
+import * as R from 'ramda'
 
 interface Item {
   created: string
 }
 
-const groupByMonth = <T extends Item>(items: T[]) =>
-  groupWith((a, b) => isSameMonth(new Date(a.created), new Date(b.created)), items)
+const groupByMonth = <T extends Item>(items: T[]) => {
+  const orderedItems = R.sort(
+    R.descend(item => item.created || ''),
+    items,
+  )
+
+  return R.groupWith((a, b) => isSameMonth(new Date(a.created), new Date(b.created)), orderedItems)
+}
 
 export default groupByMonth
